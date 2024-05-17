c1c538dd18dc30e07fea9c9d8a2adff4fc681b2a
AnuragiTriangle <anurag@itriangle.in>
2024-05-17 19:12:09 +0530

    added fontaweosme icons in sidebar

--- src/components/Sidebar/SidebarList.js
+++ src/components/Sidebar/SidebarList.js
@@ -1,17 +1,25 @@
 import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const SidebarList = () => {
 
     return (
         <ul>
             <li className="mb-2">
-                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">Dashboard</a>
+                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">
+                    <i className="fas fa-tachometer-alt mr-2"></i>
+                    Dashboard
+                </a>
             </li>
             <li className="mb-2">
-                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">Menu Item 2</a>
+                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">
+                    Item
+                </a>
             </li>
             <li className="mb-2">
-                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">Menu Item 3</a>
+                <a href="#" className="block hover:bg-gray-900 py-2 px-2 rounded">
+                    Menu Item 3
+                </a>
             </li>
         </ul>
     )
